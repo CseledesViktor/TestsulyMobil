36427d9e0ecb55a2e2b5ff04c3535fb0b4ed35a7
Cseledes <viktor.cseledes@gmail.com>
2022-05-08 19:40:24 +0200

    gogogo

---
+++ app/src/main/java/com/example/testsulyapp/regsiterActivity.java
@@ -0,0 +1,13 @@
+package com.example.testsulyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class regsiterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regsiter);
+    }
+}
